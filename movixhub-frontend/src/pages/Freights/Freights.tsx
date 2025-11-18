3a02387c1e39a4ed3bc93a5c566dc7495cdc5c12
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-11-17 21:26:15 -0300

    feat: adiciona estrutura e estilos ao cabeçalho da página de fretes

--- movixhub-frontend/src/pages/Freights/Freights.tsx
+++ movixhub-frontend/src/pages/Freights/Freights.tsx
@@ -1,10 +1,22 @@
-import styles from './Freights.module.css';
+import Button from '../../components/Button/Button';
+import Styles from './Freights.module.css';
 
 function Freights() {
+
+    const handleClick = () => {
+        alert('Clicou no botão!');
+    };
+
     return (
-        <div className={styles.freightsContainer}>
-            <h1>Serviços de Frete</h1>
-            <p>Gerencie todos os serviços de frete da Movix</p>
+        <div className={Styles.freightsContainer}>
+
+            <header className={Styles.freightsHeader}>
+                <div className={Styles.freightsHeaderDetails}>
+                    <h1>Serviços de Frete</h1>
+                    <p>Gerencie todos os serviços de frete da Movix</p>
+                </div>
+                <Button onClick={handleClick}> <i className="bi bi-plus-lg"></i> Novo Serviço</Button>
+            </header>
         </div>
     )
 }
