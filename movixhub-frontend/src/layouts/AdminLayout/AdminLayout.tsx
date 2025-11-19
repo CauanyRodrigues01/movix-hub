8f9d97e6db2620828ef3a2570a762bfa725cb080
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-11-19 12:44:34 -0300

    fix: corrige importação do componente Header para MainHeader

--- movixhub-frontend/src/layouts/AdminLayout/AdminLayout.tsx
+++ movixhub-frontend/src/layouts/AdminLayout/AdminLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Styles from "./AdminLayout.module.css";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import Header from "../../components/Header/Header";
+import Header from "../../components/MainHeader/MainHeader";
 import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
 
