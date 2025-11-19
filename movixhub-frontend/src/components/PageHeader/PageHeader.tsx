0bfdbf396cd3cdaaaec1a3ed1f882b9c6fe22407
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-11-19 12:44:19 -0300

    feat: adiciona componente PageHeader com estilo e estrutura

---
+++ movixhub-frontend/src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,26 @@
+import Button from '../Button/Button';
+import Styles from './PageHeader.module.css';
+
+interface PageHeaderProps {
+    title: string;
+    description: string;
+    buttonIcon: React.ReactNode;
+    buttonText: string;
+    onButtonClick: () => void;
+}
+
+const PageHeader = ({ title, description, buttonIcon, buttonText, onButtonClick }: PageHeaderProps) => {
+
+    return (
+        <header className={Styles.pageHeader}>
+            <div className={Styles.pageHeaderDetails}>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </div>
+            <Button onClick={onButtonClick}> {buttonIcon} {buttonText}</Button>
+        </header>
+    )
+
+}
+
+export default PageHeader;
