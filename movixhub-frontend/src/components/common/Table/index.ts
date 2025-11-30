73628d6c35e375e1d01732cbeefe075abc318fb7
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-11-30 00:49:18 -0300

    feat: adiciona novo arquivo de índice para exportação de componentes de tabela

---
+++ movixhub-frontend/src/components/common/Table/index.ts
@@ -0,0 +1,10 @@
+export { Table } from './Table';
+export { TableBadge } from './TableBadge';
+export { TableArrayRenderer } from './TableArrayRenderer';
+export { TableActions } from './TableActions';
+
+
+export { type TableProps, type ColumnDefinition } from './Table';
+export { type TableBadgeProps } from './TableBadge';
+export { type TableArrayRendererProps } from './TableArrayRenderer';
+export { type TableActionsProps } from './TableActions';
