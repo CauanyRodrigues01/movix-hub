6a8f9e9f6c160ba51fa7fdabc943afb837b98d38
brunaxmello <bruna.mello@maisunifacisa.com.br>
2026-03-14 18:31:14 -0300

    feat: add client CRUD (model, controller, routes)

--- movixhub-backend/src/config/db.ts
+++ movixhub-backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         // Adiciona opções de conexão para evitar warnings
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/movixhubdb');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'np');
         
         console.log(`MongoDB Conectado: ${conn.connection.host}`);
         
